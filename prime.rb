93ff4f06d1264bf313100fd2c6cee47adec478d3
Winston  Chung <18.chung.winston@ctkprep.org>
2019-07-24 04:42:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,6 @@
 def prime?(arg)
-  if arg == -1 
-    return false
-  elsif arg == 0
-    return false
-  elsif arg == 1
-    return false
+  if arg <=1
+    false
   else 
   (2...arg).none? {|div| arg % div == 0}
   end
